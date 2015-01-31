4266eb0dcd879df71f4e9456ee7e45cf47048717
DepthDeluxe <cheinzmann3@gmail.com>
2015-01-31 00:01:28 -0500

    runs build all with gcc

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,7 @@
-# build the compiler
+# build the compiler with GCC since this is
+# what the final thing will be built with
 pushd compiler
-make
+make gcc
 
 if [ $? -ne 0 ]; then
   echo "Error: failed to build"
